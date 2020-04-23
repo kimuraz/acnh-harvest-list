cc6717852edb20c8e74355c79166d971d2dfbf32
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2020-04-23 22:33:58 +0200

    Add DIY registries

---
+++ app/db/diy.js
@@ -0,0 +1,534 @@
+export default [
+  {
+    name: 'Flimsy axe',
+    price: '200',
+    materials: [
+      {
+        quantity: 5,
+        materialName: 'Tree branch',
+      },
+      {
+        quantity: 1,
+        materialName: 'Stone',
+      },
+    ],
+  },
+  {
+    name: 'Stone axe',
+    price: '560',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'Flimsy axe',
+      },
+      {
+        quantity: 3,
+        materialName: 'Wood',
+      },
+    ],
+  },
+  {
+    name: 'Axe',
+    price: '625',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'Flimsy axe',
+      },
+      {
+        quantity: 3,
+        materialName: 'Wood1x iron nugget',
+      },
+    ],
+  },
+  {
+    name: 'Golden axe',
+    price: '10655',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'Axe',
+      },
+      {
+        quantity: 1,
+        materialName: 'Gold nugget',
+      },
+    ],
+  },
+  {
+    name: 'Flimsy shovel',
+    price: '200',
+    materials: [
+      {
+        quantity: 5,
+        materialName: 'Hardwood',
+      },
+    ],
+  },
+  {
+    name: 'Shovel',
+    price: '600',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'Flimsy shovel',
+      },
+      {
+        quantity: 1,
+        materialName: 'Iron nugget',
+      },
+    ],
+  },
+  {
+    name: 'Golden shovel',
+    price: '10675',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'Shovel',
+      },
+      {
+        quantity: 1,
+        materialName: 'Gold nugget',
+      },
+    ],
+  },
+  {
+    name: 'Flimsy fishing rod',
+    price: '100',
+    materials: [
+      {
+        quantity: 5,
+        materialName: 'Tree branch',
+      },
+    ],
+  },
+  {
+    name: 'Fishing rod',
+    price: '600',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'Flimsy fishing rod',
+      },
+      {
+        quantity: 1,
+        materialName: 'Iron nugget',
+      },
+    ],
+  },
+  {
+    name: 'Golden rod',
+    price: '10400',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'Fishing rod',
+      },
+      {
+        quantity: 1,
+        materialName: 'Gold nugget',
+      },
+    ],
+  },
+  {
+    name: 'Flimsy net',
+    price: '100',
+    materials: [
+      {
+        quantity: 5,
+        materialName: 'Tree branch',
+      },
+    ],
+  },
+  {
+    name: 'Net',
+    price: '600',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'Flimsy net',
+      },
+      {
+        quantity: 1,
+        materialName: 'Iron nugget',
+      },
+    ],
+  },
+  {
+    name: 'Golden net',
+    price: '10400',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'Net',
+      },
+      {
+        quantity: 1,
+        materialName: 'Gold nugget',
+      },
+    ],
+  },
+  {
+    name: 'Flimsy watering can',
+    price: '200',
+    materials: [
+      {
+        quantity: 5,
+        materialName: 'Softwood',
+      },
+    ],
+  },
+  {
+    name: 'Watering can',
+    price: '600',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'Flimsy watering can',
+      },
+      {
+        quantity: 1,
+        materialName: 'Iron nugget',
+      },
+    ],
+  },
+  {
+    name: 'Golden watering can',
+    price: '10675',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'Watering can',
+      },
+      {
+        quantity: 1,
+        materialName: 'Gold nugget',
+      },
+    ],
+  },
+  {
+    name: 'Slingshot',
+    price: '225',
+    materials: [
+      {
+        quantity: 5,
+        materialName: 'Hardwood',
+      },
+    ],
+  },
+  {
+    name: 'Golden slingshot',
+    price: '10300',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'Slingshot',
+      },
+      {
+        quantity: 1,
+        materialName: 'Gold nugget',
+      },
+    ],
+  },
+  {
+    name: 'Vaulting pole',
+    price: '600',
+    materials: [
+      {
+        quantity: 5,
+        materialName: 'Softwood',
+      },
+    ],
+  },
+  {
+    name: 'Ladder',
+    price: '1440',
+    materials: [
+      {
+        quantity: 4,
+        materialName: 'Wood',
+      },
+      {
+        quantity: 4,
+        materialName: 'Hardwood4x softwood',
+      },
+    ],
+  },
+  {
+    name: 'Star wand',
+    price: '6500',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'Large star fragment',
+      },
+      {
+        quantity: 3,
+        materialName: 'Star fragment',
+      },
+    ],
+  },
+  {
+    name: 'Bamboo wand',
+    price: '3900',
+    materials: [
+      {
+        quantity: 6,
+        materialName: 'Young spring bamboo',
+      },
+      {
+        quantity: 3,
+        materialName: 'Star fragment',
+      },
+    ],
+  },
+  {
+    name: 'Mushroom wand',
+    price: '3300',
+    materials: [
+      {
+        quantity: 3,
+        materialName: 'Skinny mushroom',
+      },
+      {
+        quantity: 3,
+        materialName: 'Star fragment',
+      },
+    ],
+  },
+  {
+    name: 'Cherry-blossom wand',
+    price: '2700',
+    materials: [
+      {
+        quantity: 3,
+        materialName: 'Cherry-blossom petal',
+      },
+      {
+        quantity: 3,
+        materialName: 'Star fragment',
+      },
+    ],
+  },
+  {
+    name: 'Bunny Day wand',
+    price: '11100',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'Wobbling Zipper toy',
+      },
+      {
+        quantity: 3,
+        materialName: 'Star fragment',
+      },
+    ],
+  },
+  {
+    name: 'Cosmos wand',
+    price: '1580',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'White cosmos',
+      },
+      {
+        quantity: 3,
+        materialName: 'Star fragment',
+      },
+    ],
+  },
+  {
+    name: 'Hyacinth wand',
+    price: '1660',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'Pink hyacinths',
+      },
+      {
+        quantity: 3,
+        materialName: 'Star fragment',
+      },
+    ],
+  },
+  {
+    name: 'Lily wand',
+    price: '1580',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'White lilies',
+      },
+      {
+        quantity: 3,
+        materialName: 'Star fragment',
+      },
+    ],
+  },
+  {
+    name: 'Mums wand',
+    price: '1580',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'Yellow mums',
+      },
+      {
+        quantity: 3,
+        materialName: 'Star fragment',
+      },
+    ],
+  },
+  {
+    name: 'Pansy Wand',
+    price: '1580',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'Yellow pansies',
+      },
+      {
+        quantity: 3,
+        materialName: 'Star fragment',
+      },
+    ],
+  },
+  {
+    name: 'Rose wand',
+    price: '1580',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'Red roses',
+      },
+      {
+        quantity: 3,
+        materialName: 'Star fragment',
+      },
+    ],
+  },
+  {
+    name: 'Tulip wand',
+    price: '1580',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'Red tulips',
+      },
+      {
+        quantity: 3,
+        materialName: 'Star fragment',
+      },
+    ],
+  },
+  {
+    name: 'Windflower wand',
+    price: '1580',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'Orange windflowers',
+      },
+      {
+        quantity: 3,
+        materialName: 'Star fragment',
+      },
+    ],
+  },
+  {
+    name: 'Shell wand',
+    price: '5100',
+    materials: [
+      {
+        quantity: 3,
+        materialName: 'Summer shell',
+      },
+      {
+        quantity: 3,
+        materialName: 'Star fragment',
+      },
+    ],
+  },
+  {
+    name: 'Ice wand',
+    price: '6500',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'Large snowflake',
+      },
+      {
+        quantity: 3,
+        materialName: 'Star fragment',
+      },
+    ],
+  },
+  {
+    name: 'Golden wand',
+    price: '20750',
+    materials: [
+      {
+        quantity: 2,
+        materialName: 'Gold nugget ',
+      },
+      {
+        quantity: 3,
+        materialName: 'Star fragment',
+      },
+    ],
+  },
+  {
+    name: 'Wand',
+    price: '1000',
+    materials: [
+      {
+        quantity: 2,
+        materialName: 'Star fragment',
+      },
+    ],
+  },
+  {
+    name: 'Tree-branch wand',
+    price: '1550',
+    materials: [
+      {
+        quantity: 5,
+        materialName: 'Tree branch',
+      },
+      {
+        quantity: 3,
+        materialName: 'Star fragment',
+      },
+    ],
+  },
+  {
+    name: 'Iron wand',
+    price: '3750',
+    materials: [
+      {
+        quantity: 3,
+        materialName: 'Iron nugget',
+      },
+      {
+        quantity: 3,
+        materialName: 'Star fragment',
+      },
+    ],
+  },
+  {
+    name: 'Wedding wand',
+    price: '3500',
+    materials: [
+      {
+        quantity: 1,
+        materialName: 'Wedding flower stand',
+      },
+      {
+        quantity: 3,
+        materialName: 'Star fragment',
+      },
+    ],
+  },
+];
