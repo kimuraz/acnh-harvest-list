edbb769e6db26d2a03f7d82ca370c0f53dfb17cb
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2020-04-24 00:17:13 +0200

    Add handling for harvest list on appsettings

--- app/components/App.vue
+++ app/components/App.vue
@@ -12,17 +12,13 @@
       androidSelectedTabHighlightColor="#ffffff"
     >
       <TabViewItem title="All materials">
-        <MaterialsList />
+        <MaterialsList @addMaterial="addMaterial" />
       </TabViewItem>
-      <TabViewItem title="Furniture">
-        <GridLayout columns="*" rows="*">
-          <Label class="message" text="Tab 2 Content" col="0" row="0" />
-        </GridLayout>
+      <TabViewItem title="DIY" @addMaterial="addMaterial">
+        <DIYList />
       </TabViewItem>
       <TabViewItem title="Harvest list">
-        <GridLayout columns="*" rows="*">
-          <Label class="message" text="Tab 3 Content" col="0" row="0" />
-        </GridLayout>
+        <HarvestList :list="list" />
       </TabViewItem>
     </TabView>
   </Page>
@@ -30,14 +26,39 @@
 
 <script>
 import MaterialsList from './MaterialsList';
+import DIYList from './DIYList';
+import HarvestList from './HarvestList';
+
+const appSettings = require('tns-core-modules/application-settings');
+
 export default {
   components: {
     MaterialsList,
+    DIYList,
+    HarvestList,
   },
   data() {
     return {
-      msg: 'Hello World!',
+      list: {},
     };
+  },
+  created() {
+    this.list = JSON.parse(appSettings.getString('list', '{}'));
+  },
+  methods: {
+    addMaterial(material) {
+      if (this.list[material.name]) {
+        this.list[material.name] += material.amount;
+      } else {
+        this.$set(this.list, material.name, material.amount);
+      }
+      appSettings.setString('list', JSON.stringify(this.list));
+    },
+    removeMaterial(material, amount) {
+      const curAmount = this.list[material.name];
+      this.list[material.name] =
+        curAmount - amount < 0 ? 0 : curAmount - amount;
+    },
   },
 };
 </script>
