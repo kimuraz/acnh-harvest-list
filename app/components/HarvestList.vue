a0c77a0e1164d8b23f5cc991cd54a8a7f0f80a95
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2020-04-24 01:39:05 +0200

    Add harvest list without controls

---
+++ app/components/HarvestList.vue
@@ -0,0 +1,22 @@
+<template>
+  <StackLayout>
+    <ListView for="harvest in Object.keys(list)" style="height: 90%">
+      <v-template>
+        <Label class="h4" :text="harvest" padding="20" />
+      </v-template>
+    </ListView>
+    <Button text="Clear all" class="red-bg" @tap="$emit('removeAll')"/>
+  </StackLayout>
+</template>
+
+<script>
+export default {
+  name: 'HarvestList',
+  props: {
+    list: {
+      required: true,
+      type: Object,
+    },
+  },
+};
+</script>
